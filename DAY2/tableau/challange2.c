e0e3ecee157c8dc24c6116b86a2c432ee97f8c24
unknown <boubkerhabib41@gmal.com>
2025-09-05 09:36:36 +0200

    SAS2025

--- DAY2/tableau/challange2.c
+++ DAY2/tableau/challange2.c
@@ -1,10 +1,8 @@
-
 #include <stdio.h>
 #include <string.h>
 
 int x,y,z,i,j,r,h,u;
-int T[5] = {1,2,3,4,5};
-
+int T[5];
 int main(){
     //printf("entrer n:");
     //scanf("%d",&j);
@@ -12,19 +10,10 @@ int main(){
     for (i = 0;i < 5;i++){
             printf("entrer T[%d] = :",i+1);
             scanf("%d",&T[i]);
-
     }
     for (i = 0;i < 5;i++){
             printf("T[%d] = %d:\n",i+1,T[i]);
 
     }
-
-
-
-
-
-
-
     return 0;
-
 }
